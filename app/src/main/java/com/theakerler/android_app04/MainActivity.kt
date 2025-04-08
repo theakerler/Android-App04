bb174a1d912087cbfd320ce67c42f9e20a25d155
Alexander EP <palex2143@gmail.com>
2025-04-08 14:52:56 -0500

    feat: Add ViewHolaCurso

--- app/src/main/java/com/theakerler/android_app04/MainActivity.kt
+++ app/src/main/java/com/theakerler/android_app04/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -30,9 +31,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.theakerler.android_app04.ui.theme.AndroidApp04Theme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,9 +72,33 @@ fun GreetingPreview() {
     AndroidApp04Theme {
 //        NavigationExample()
 //        TabEjemplo()
-        CampoTextoEjemplo()
+//        CampoTextoEjemplo()
+        ViewHolaCurso()
     }
 }
+
+@Composable
+fun ViewHolaCurso() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Course!",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Hello, Student!",
+            fontSize = 28.sp,
+        )
+    }
+}
+
+
 
 @Composable
 fun CampoTextoEjemplo() {
@@ -80,6 +111,7 @@ fun CampoTextoEjemplo() {
         modifier = Modifier.fillMaxWidth()
     )
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavigationExample() {
